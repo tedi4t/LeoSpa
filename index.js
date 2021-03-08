fd425800d7432f88c5f9406be6df97807092bee0
dmytro <tedi4t@gmail.com>
2021-03-08 20:38:15 +0200

    add map

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 $('.navbar-small-devices').append($('.nav-collapse').children().clone())
 
 $('.navbar-toggler').click(() => {
-  console.log('click!');
   $('.navbar-small-devices').toggleClass('active');
+})
 
+$('path').hover(function(){
+  var element = $(this);
+  var attributes = {};
+  $.each(element.get(0).attributes, function(i, attrib){
+  attributes[attrib.name] = attrib.value;
+  });
+  const name = attributes.name || attributes.class;
+
+  $('#map-country-name').html(name);
+  $('#map-country-name').css('opacity', '1');
+  
+  $(this).mousemove(function(event) {
+    const clientX = event.clientX;
+    const clientY = event.clientY;
+    $('#map-country-name').css({
+      'top': `${clientY + 20}px`,
+      'left': `${clientX + 20}px`
+    });
+  });
+});
+
+$('path').mouseout(function() {
+  $('#map-country-name').css('opacity', '0');
 })
